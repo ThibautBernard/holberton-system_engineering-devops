36cf7415ac9365c0ec04205a0d551db34063e36e
ThibautBernard <thibautbernard@sfr.fr>
2021-04-04 15:17:38 +0000

    fix: add file to etc/ssh/

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # add to config file of SSH to turn of passwd at auth 
 # and declare the path of key to use
 file_line { 'Turn off passwd auth':
-    path => '/etc/ssh/',
+    path => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no',
 }
 file_line { 'Declare identity file':
-    path => '/etc/ssh/',
+    path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/holberton',
 }
