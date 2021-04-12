c36086dac03f54e5af2e6824b434eff35f1814f9
ThibautBernard <thibautbernard@sfr.fr>
2021-04-12 14:45:05 +0000

    fix: add require nginx or install

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,35 @@
 # add custom header
 # $h = generate ('/bin/bash', '$HOSTNAME')
 # exec { 'update value page':
-#    command  => 'sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n 
+#    command  => 'sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n
 # add_header X-Served-By ${blah} ;/" /etc/nginx/sites-available/default',
 #    provider => 'shell',
 # }
+
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt'
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => 'Holberton School for the win!'
+}
+
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '\trewrite ^/redirect_me https://www.twitch.com permanent;',
+}
 
 $d = $::hostname
 file_line { 'add_header':
@@ -12,4 +37,8 @@ file_line { 'add_header':
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => "\tadd_header X-Served-By ${d};",
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
