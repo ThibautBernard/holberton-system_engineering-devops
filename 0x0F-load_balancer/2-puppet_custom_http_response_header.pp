1b6574bebfc2495c60b46d69eab472304b760067
ThibautBernard <thibautbernard@sfr.fr>
2021-04-12 11:33:08 +0000

    feat

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,7 @@
+# add custom header
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $HOSTNAME;',
+}
