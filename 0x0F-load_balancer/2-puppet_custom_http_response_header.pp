fbab6a6d0a7670f6a83cea23bb11c0bf4d4905cc
ThibautBernard <thibautbernard@sfr.fr>
2021-04-12 11:42:45 +0000

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,8 @@
 # add custom header
+$h = generate ('/bin/bash', '-c', 'echo $HOSTNAME')
 file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $HOSTNAME;',
+  line   => 'add_header X-Served-By $(h);',
 }
