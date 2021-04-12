596bcc821d9f25b0f395638172054a39b5345d7b
ThibautBernard <thibautbernard@sfr.fr>
2021-04-12 14:53:56 +0000

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -21,7 +21,7 @@ file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'root /var/www/html;',
-  line   => '   rewrite ^/redirect_me https://www.twitch.com permanent;',
+  line   => 'rewrite ^/redirect_me https://www.twitch.com permanent;',
 }
 
 $d = $::hostname
@@ -29,7 +29,7 @@ file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => "   add_header X-Served-By ${d};",
+  line   => "add_header X-Served-By ${d};",
 }
 service { 'nginx':
   ensure  => running,
