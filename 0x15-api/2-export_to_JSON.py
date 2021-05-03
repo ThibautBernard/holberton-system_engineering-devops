04b5f91e99a899e64cb8ffd3f1cd7949b553b12d
ThibautBernard <thibautbernard@sfr.fr>
2021-05-03 15:37:27 +0000

    fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,25 +9,24 @@ import sys
 
 
 if __name__ == "__main__":
-    req = "https://jsonplaceholder.typicode.com/users/{}/todos"\
-           .format(sys.argv[1])
-    response = requests.get(req).json()
-    total_tasks = len(response)
-    task_completed = [i for i, k in enumerate(response) if k['completed']]
-    req_use = "https://jsonplaceholder.typicode.com/users/{}"\
+    if len(sys.argv) == 2:
+        req = "https://jsonplaceholder.typicode.com/users/{}/todos"\
               .format(sys.argv[1])
-    info_user = requests.get(req_use).json()
-    name_user = info_user['username']
-    d = {}
-    list_tasks = []
-    for content in response:
-        d_task = {}
-        task = "{}".format(content['title'])
-        complet = "{}".format(content['completed'])
-        d_task['task'] = task
-        d_task['completed'] = complet
-        d_task['username'] = name_user
-        list_tasks.append(d_task)
-    d[info_user['id']] = list_tasks
-    with open('{}.json'.format(sys.argv[1]), 'w') as f:
-        json.dump(d, f)
+        response = requests.get(req).json()
+        req_use = "https://jsonplaceholder.typicode.com/users/{}"\
+                  .format(sys.argv[1])
+        info_user = requests.get(req_use).json()
+        name_user = info_user['username']
+        d = {}
+        list_tasks = []
+        for content in response:
+            d_task = {}
+            task = "{}".format(content['title'])
+            complet = "{}".format(content['completed'])
+            d_task['task'] = task
+            d_task['completed'] = complet
+            d_task['username'] = name_user
+            list_tasks.append(d_task)
+        d[sys.argv[1]] = list_tasks
+        with open('{}.json'.format(sys.argv[1]), 'w') as f:
+            json.dump(d, f)
