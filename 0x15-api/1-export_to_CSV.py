1b4b8e98668ef9885fdefe922775c7ee5d5657f4
ThibautBernard <thibautbernard@sfr.fr>
2021-05-03 10:29:29 +0000

    feat

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    request api and write in csv file
+    all tasks todo for an user
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+           .format(sys.argv[1])
+    response = requests.get(req).json()
+    total_tasks = len(response)
+    task_completed = [i for i, k in enumerate(response) if k['completed']]
+    req_use = "https://jsonplaceholder.typicode.com/users/{}"\
+              .format(sys.argv[1])
+    info_user = requests.get(req_use).json()
+    name_user = info_user['name']
+    with open('{}.csv'.format(info_user['id']), 'w', newline='') as file:
+        for content in response:
+            writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+            id_us = "{}".format(content['userId'])
+            complet = "{}".format(content['completed'])
+            content = "{}".format(content['title'])
+            writer.writerow([id_us, name_user, complet, content])
