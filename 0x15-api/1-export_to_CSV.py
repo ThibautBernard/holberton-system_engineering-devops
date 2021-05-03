ab6f322c4acadef06486321a33efb5d0a209ef8d
ThibautBernard <thibautbernard@sfr.fr>
2021-05-03 15:13:17 +0000

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,10 +18,10 @@ if __name__ == "__main__":
               .format(sys.argv[1])
     info_user = requests.get(req_use).json()
     name_user = info_user['name']
-    with open('{}.csv'.format(info_user['id']), 'w', newline='') as file:
+    with open('{}.csv'.format(info_user['id']), 'w') as file:
         for content in response:
             writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
-            id_us = "{}".format(content['userId'])
-            complet = "{}".format(content['completed'])
-            content = "{}".format(content['title'])
+            id_us = content['userId']
+            complet = content['completed']
+            content = content['title']
             writer.writerow([id_us, name_user, complet, content])
