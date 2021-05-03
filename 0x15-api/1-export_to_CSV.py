8cfa77f7bea8f83c30e9f0ba429f5863c344f222
ThibautBernard <thibautbernard@sfr.fr>
2021-05-03 15:15:58 +0000

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,15 +12,13 @@ if __name__ == "__main__":
     req = "https://jsonplaceholder.typicode.com/users/{}/todos"\
            .format(sys.argv[1])
     response = requests.get(req).json()
-    total_tasks = len(response)
-    task_completed = [i for i, k in enumerate(response) if k['completed']]
     req_use = "https://jsonplaceholder.typicode.com/users/{}"\
               .format(sys.argv[1])
     info_user = requests.get(req_use).json()
     name_user = info_user['name']
     with open('{}.csv'.format(info_user['id']), 'w') as file:
         for content in response:
-            writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
             id_us = content['userId']
             complet = content['completed']
             content = content['title']
