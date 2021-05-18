e4e8debe686da4afb2d18f8466a200e063778f78
ThibautBernard <thibautbernard@sfr.fr>
2021-05-18 11:17:40 +0000

    feat: task 3

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    request api reddit and return a list of all
+    hot article recursively
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], counter=0, after='t3_'):
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=1&after={}'\
+         .format(subreddit, after)
+    headers = {'user-agent': 'my-app/0.0.1'}
+    result = requests.get(url, headers=headers)
+    hot_list.append(result.json()['data']['children'][0]['data']['title'])
+    if result.status_code == 404 or len(result.json()) == 0:
+        return
+    else:
+        counter += 1
+        after = result.json()['data']['after']
+        recurse(subreddit, hot_list, counter, after)
+        return hot_list
